2fdc5854d12e3357a9322384e17d58853855e20b
Wojciech Piotrowiak <wojciech.piotrowiak@gmail.com>
2016-10-12 22:11:45 +0200

    simple graphs + persistance properties

--- src/main/java/main/MeasureController.java
+++ src/main/java/main/MeasureController.java
@@ -3,6 +3,9 @@ package main;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import main.storage.PressureRepository;
 import main.storage.TemperatureRepository;
@@ -55,20 +58,30 @@ public class MeasureController {
 
     @RequestMapping("/graph")
     void getGraph(HttpServletResponse response) throws IOException {
-        String values="";
         int count=0;
         String c="|";
+        List<Temperature> temperatureList=new ArrayList<>();
         for (Temperature t : temperatureRepository.findAll()) {
-            values+=","+t.getValue();
+            temperatureList.add(t);
             c+=count+"|";
             count++;
         }
-        values=values.substring(1);
+        String values = temperatureList.stream().map(t -> t.getValue()).collect(Collectors.joining(","));
+        String minMax = getMinMax(temperatureList);
+
+
         //http://chart.apis.google.com/chart?chs=500x300&cht=lc&chd=t:26.60,26.60,26.30,26.60,26.60
         // &chds=26.30,26.60&chco=FF0000&chls=6&chxt=x,y&chxl=0:|0|1|2|3|4|1:||15|20|30&chf=bg,s,efefef
         response.sendRedirect("http://chart.apis.google.com/chart?chs=500x300" +
-                "&cht=lc&chd=t:" +values+
+                "&cht=lc&chd=t:" +values+minMax+
                 "&chco=FF0000&chls=6&chxt=x,y&chxl=0:"+c+"1:||15|20|30&chf=bg,s,efefef");
+    }
+
+    private String getMinMax(List<Temperature> temperatureList) {
+        String minMax="&chds=";
+        DoubleSummaryStatistics stat = temperatureList.stream().mapToDouble(t -> Double.valueOf(t.getValue())).summaryStatistics();
+        minMax+=stat.getMin()+","+stat.getMax();
+        return minMax;
     }
 
     @Scheduled(cron = "0 */10 * * * *")
