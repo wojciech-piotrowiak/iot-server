a3879f7ab2a816c33b01414e4cbe3a9741df9b3c
Wojciech Piotrowiak <wojciech.piotrowiak@gmail.com>
2016-11-09 22:59:41 +0100

    possibility to create a running jar

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -2,11 +2,12 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/main/Mvc.java
+++ src/main/java/main/Mvc.java
@@ -1,14 +1,60 @@
 package main;
 
+import main.storage.PressureRepository;
+import main.storage.TemperatureRepository;
+import main.storage.pojo.Pressure;
+import main.storage.pojo.Temperature;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class Mvc {
 
-    @RequestMapping( value="/index" , method = RequestMethod.GET  )
-    public String main(){
+    @Autowired
+    private TemperatureRepository temperatureRepository;
+
+    @Autowired
+    private PressureRepository pressureRepository;
+
+
+    @RequestMapping("/")
+    public String listProducts(Model model){
+
+        model.addAttribute("temperature", getTemp());
+        model.addAttribute("pressure", getPressure());
+
         return "graphs";
     }
+
+    public String getTemp() {
+        List<String> values = new ArrayList<>();
+        for (Temperature t : temperatureRepository.findAll()) {
+            values.add(String.format("['%s', %s]", getDate(t.getDate()), t.getValue()));
+        }
+
+        return String.format("[%s]",values.stream().collect(Collectors.joining(",")));
+    }
+
+    public String getPressure() {
+        List<String> values = new ArrayList<>();
+        for (Pressure p : pressureRepository.findAll()) {
+            values.add(String.format("['%s', %s]", getDate(p.getDate()), p.getValue()));
+        }
+
+        return String.format("[%s]",values.stream().collect(Collectors.joining(",")));
+    }
+
+    private String getDate(String t) {
+        ZonedDateTime parse = ZonedDateTime.parse(t).plusHours(2);
+        return parse.getDayOfWeek() + " " + parse.getHour() + ":" + parse.getMinute();
+    }
+
+
 }
